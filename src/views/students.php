48ea93abb6134ee8936c4eb802816b83c2201bfd
karolkusper <karolkusper@gmail.com>
2024-01-21 20:49:50 +0100

    add shared header in students.php

--- src/views/students.php
+++ src/views/students.php
@@ -1,20 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <title>Users</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com"/>
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-    <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;500&family=Poppins:wght@400;600&display=swap"
-            rel="stylesheet"
-    />
-    <script
-            src="https://kit.fontawesome.com/415b20756c.js"
-            crossorigin="anonymous"
-    ></script>
-    <link rel="stylesheet" href="public/css/common_styles.css"/>
+    <?php include(__DIR__ . "/shared/header.php"); ?>
+    <title>Students</title>
     <link rel="stylesheet" href="public/css/users.css"/>
 </head>
 <body>
